cefffb62d0f369491ecb110104a3449c0f9e21ef
Arrnaud TIERANT <arnaud@tierant.fr>
2023-03-29 11:14:51 +0200

    fix(sitemap): Add / at the end of each sitemap url to avoid redirection

---
+++ src/Command/SitemapTransformerCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use DOMDocument;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:transform-sitemap',
+    description: 'Add a short description for your command',
+)]
+class SitemapTransformerCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('file', InputArgument::REQUIRED, 'Argument description');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $file = $input->getArgument('file');
+
+        // Load the XML source
+        $xml = new DOMDocument();
+        $xml->load($file);
+
+        $root = $xml->documentElement; // This can differ (I am not sure, it can be only documentElement or documentElement->firstChild or only firstChild)
+
+        $urls = $root->getElementsByTagName('url');
+
+        // Loop trough childNodes
+        foreach ($urls as $url) {
+            $loc = $url->getElementsByTagName('loc')->item(0);
+            $value = $loc->nodeValue;
+            $ext = pathinfo($value, PATHINFO_EXTENSION);
+            if (in_array($ext, ['xml'], true)) {
+                continue;
+            }
+
+            if (str_ends_with($value, '/')) {
+                continue;
+            }
+
+            $loc->nodeValue = $value . '/';
+
+            $url->replaceChild($loc, $loc);
+        }
+
+        $xml->save($file);
+
+        return Command::SUCCESS;
+    }
+}
