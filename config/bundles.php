d268f70b79411f1fab5982a15f9e34b27bc76065
Arrnaud TIERANT <arnaud@tierant.fr>
2024-01-15 17:36:28 +0100

    fix(ci): Update dependabot scans

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    //Symfony\UX\Swup\SwupBundle::class => ['all' => true],
 ];
