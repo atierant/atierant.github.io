3ed6b4a40f51f881504bbc31e567adb4dedba7c8
Arrnaud TIERANT <arnaud@tierant.fr>
2024-01-15 17:54:44 +0100

    fix(ci): Update dependabot scans

--- config/bundles.php
+++ config/bundles.php
@@ -6,12 +6,10 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Stenope\Bundle\StenopeBundle::class => ['all' => true],
     App\Bridge\Glide\Bundle\GlideBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
-    //Symfony\UX\Swup\SwupBundle::class => ['all' => true],
 ];
